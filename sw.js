7cf08e3b8b92dbfec70707dc80721d9e2cfe8462
GeoLeonardo <158768965+GeoLeonardo@users.noreply.github.com>
2026-03-20 14:08:47 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const V='vc3';
-const F=['/','index.html','manifest.json','icon.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(F)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
