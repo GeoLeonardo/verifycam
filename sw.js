7963ec6fbddbe859f5bdb6a17a4c7c66cf71f780
GeoLeonardo <158768965+GeoLeonardo@users.noreply.github.com>
2026-03-20 23:21:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+var C='vcam2';
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(C).then(function(cache){
+    return Promise.all(['./index.html','./manifest.json','./icon.png','./config.json'].map(function(f){
+      return cache.add(f).catch(function(){});
+    }));
+  }).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){
+    return Promise.all(ks.filter(function(k){return k!==C;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(caches.match(e.request).then(function(r){
+    if(r)return r;
+    return fetch(e.request).then(function(res){
+      if(res&&res.status===200){caches.open(C).then(function(c){c.put(e.request,res.clone());});}
+      return res;
+    }).catch(function(){return new Response('',{status:503});});
+  }));
+});
